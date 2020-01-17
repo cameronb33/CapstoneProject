764506083b496ab0bcd95cb360402677086f5e15
Brandon <cameronb33@mymacewan.ca>
2020-01-17 02:07:43 -0700

    Revert "Contains the databse connection"
    This reverts commit c96d30f7496b6c978f377ecea49fdd9adbf9b1d2.

--- CapstoneProject/CapstoneProject/App.xaml.cs
+++ CapstoneProject/CapstoneProject/App.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using FireSharp.Config;
-using FireSharp.Interfaces;
-using FireSharp.Response;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace CapstoneProject
@@ -13,18 +10,7 @@ namespace CapstoneProject
         public App()
         {
             InitializeComponent();
-            // Pathname and api key for the firestore database 
-            IFirebaseConfig config = new FirebaseConfig
-            {
-                AuthSecret = "8XYiiyg63vVWHlgkJeQBoFTrRZTZXoL3oUW84m8u",
-                BasePath = "https://capstone-6acec.firebase.io.com/"
-            };
-            IFirebaseClient client;
-            client = new FireSharp.FirebaseClient(config);
-            if(client != null)
-            {
-                Console.WriteLine("Connection is eshtablished");
-            }
+
             MainPage = new MainPage();
         }
 
