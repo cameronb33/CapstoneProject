364b19bcbebd2088f9778b165bcbafb8e669a767
Brandon <cameronb33@mymacewan.ca>
2020-02-07 02:57:49 -0700

    Clean up Task Page and make multi line buttons on Calendar Page

--- CapstoneProject/CapstoneProject/CalendarPage.xaml.cs
+++ CapstoneProject/CapstoneProject/CalendarPage.xaml.cs
@@ -12,9 +12,62 @@ namespace CapstoneProject
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CalendarPage : ContentPage
 	{
-		public CalendarPage ()
+        public IList<TaskItem> Tasks { get; private set; }
+        public CalendarPage ()
 		{
 			InitializeComponent();
+
+            Tasks = new List<TaskItem>();
+            Tasks.Add(new TaskItem
+            {
+                Location = "MacEwan",
+                Time = "8:00 AM - 9:00 AM",
+                Workers = "Brandon, Brayden"
+            });
+
+            Tasks.Add(new TaskItem
+            {
+                Location = "Edmonton",
+                Time = "9:00 AM - 9:30 AM",
+                Workers = "Brandon, Brayden"
+            });
+
+            Tasks.Add(new TaskItem
+            {
+                Location = "Windermere",
+                Time = "9:30 AM - 10:00 AM",
+                Workers = "Brandon, Brayden"
+            });
+
+            Tasks.Add(new TaskItem
+            {
+                Location = "Windermere",
+                Time = "10:00 AM - 10:30 AM",
+                Workers = "Brandon, Brayden"
+            });
+
+            Tasks.Add(new TaskItem
+            {
+                Location = "Windermere",
+                Time = "10:30 AM - 11:00 AM",
+                Workers = "Brandon, Brayden"
+            });
+
+            Tasks.Add(new TaskItem
+            {
+                Location = "MacEwan",
+                Time = "11:00 AM - 12:00 PM",
+                Workers = "Brandon, Brayden"
+            });
+
+            Tasks.Add(new TaskItem
+            {
+                Location = "Edmonton",
+                Time = "12:00 PM - 5:00 PM",
+                Workers = "Brandon, Brayden"
+            });
+
+            BindingContext = this;
         }
 
         private async void BtnList_Clicked(object sender, EventArgs e)
---
+++ CapstoneProject/CapstoneProject/TaskItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapstoneProject
+{
+    public class TaskItem
+    {
+        public string Location { get; set; }
+        public string Time { get; set; }
+        public string Workers { get; set; }
+
+        public override string ToString()
+        {
+            return Location;
+        }
+    }
+}
